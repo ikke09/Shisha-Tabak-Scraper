f27434871aec18ae83b2da489ad1f216af49d3ac
ikke09 <development@fam-bechtluft.de>
2022-01-22 16:01:13 +0100

    Implement loading product details

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 const loadHTML = async (pageNumber = 1) => {
     try {
@@ -63,9 +64,46 @@ const loadAllProductLinks = async(maxPages) => {
     return productLinks;
 }
 
+const loadProductDetails = async(productLink) => {
+    try {
+        const response = await axios.get(productLink, {
+            responseType: 'document',
+            responseEncoding: 'utf8'
+        });
+        const $ = cheerio.load(response.data);
+        const regex = new RegExp('([\\w ]+) (Tabak|Tobacco|Booster|Mix|Tabakersatz) (.+) (\\d+)(g|ml|kg)', 'g');
+        const title = $('h1.product--title').text().trim();
+        const matches = [...title.matchAll(regex)][0];
+        const { 1: producer, 2: type, 3: name, 4: amount, 5: unit } = matches;
+        const res = {
+            producer,
+            name, 
+            type,
+            amount: Number(amount), 
+            unit,
+            url: productLink
+        };
+        return res;
+    } catch (error) {
+        //console.error(error);
+        return {
+            url: productLink,
+            msg: 'Failed to load Details'
+        };
+    }
+}
+
 const scrape = async () => {
-    const {pageCount} = await loadProductCount();
+    const {pageCount, maxCount} = await loadProductCount();
     const productLinks = await loadAllProductLinks(pageCount);
+    const products = await Promise.all(productLinks.map(async(link) => await loadProductDetails(link)));
+    const successCount = products.filter(p => !p.msg).length;
+    fs.writeFile('./tobaccos.json', JSON.stringify(products), 'utf8', (err) => {
+        if (err) {
+            console.log(`Error writing file: ${err}`);
+        }
+    });
+    console.log(`Loading products done. ${successCount}/${maxCount} successfull!`);
 }
 
 scrape();
